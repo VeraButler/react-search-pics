fe5419e0bf5d069f97734035328f1d18ae912312
Vera Butler <verabutlerdesign@gmail.com>
2019-03-20 13:58:41 -0400

    add unsplash fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+import {auth} from 'config.js';
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log(term)
+        // axios fetch
+        let rootURL = 'https://api.unsplash.com';
+        axios.get(`${rootURL}/search/photos`, {
+            // create params to specify query strings
+            params: {
+                query: term
+            },
+            // create headers for making requests
+            headers: {
+                // auth
+                Authorization: `Client-ID ${auth.unsplashID}`   
+            }
+            // 
+        })
     }
 
     render() {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,14 +5,14 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (event) => {
         event.preventDefault();
-
         this.props.onSubmit(this.state.term);
     };
 
     render() {
         return (
             <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
+                <form onSubmit={this.onFormSubmit} 
+                className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input
